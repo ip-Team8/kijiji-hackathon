c6a871aaf1012d0910f5ac94ffaed16354952b7b
Sariah Conor <sariah.conor@gmail.com>
2024-04-25 20:41:45 -0700

    Add Header and Footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,11 @@
 import React from "react";
 import './Footer.scss';
+import kijijiFooter from '../../assets/images/footer.jpg';
+
 const Footer = () => {
     return (
         <>
+        <img className="footer" src={kijijiFooter}></img>
         </>
     )
 } 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
 import './Header.scss';
+import kijijiHeader from '../../assets/images/header-favourites.jpg';
+
 const Header = () => {
     return (
         <>
+        <img className="header" src={kijijiHeader} alt=""></img>
         </>
     )
 } 
